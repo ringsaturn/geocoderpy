640b1889f68b42f71e11e16a4fba00fe6e627922
ringsaturn <ringsaturn.me@gmail.com>
2023-07-31 21:46:10 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,19 @@ pub struct Info {
 
 #[pyfunction]
 fn get_info(_py: Python, lng: f64, lat: f64) -> PyResult<Info> {
-    let res = CODER.lookup(Point::new(lng, lat)).unwrap();
+    let res = CODER.lookup(Point::new(lng, lat));
+    if res.is_none() {
+        return Ok(Info {
+            code: "".to_string(),
+            name: "".to_string(),
+        });
+    }
+    let v = res.unwrap();
     return Ok(Info {
-        code: res.code.to_owned(),
-        name: res.name.to_owned(),
+        code: v.code.to_owned(),
+        name: v.name.to_owned(),
     });
 }
-
 
 #[pymodule]
 fn geocoderpy(_py: Python, m: &PyModule) -> PyResult<()> {
